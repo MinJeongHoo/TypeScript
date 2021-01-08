a623044ee9a39cd2d254d5ba04912bb21f15b692
MinJeongHoo <msw1302@gmail.com>
2021-01-08 10:49:22 +0900

    first

---
+++ hello.ts
@@ -0,0 +1,7 @@
+let hello = 'hello';
+let timeoutPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("1 sec");
+    }, 1000);
+})
+timeoutPromise.then(console.log);

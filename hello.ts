ec7c894b98307d8eeb4dc5e1fa39f810fc93baae
MinJeongHoo <msw1302@gmail.com>
2021-01-12 21:18:50 +0900

    varilable

--- hello.ts
+++
@@ -1,7 +0,0 @@
-let hello = 'hello';
-let timeoutPromise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve("1 sec");
-    }, 1000);
-})
-timeoutPromise.then(console.log);
---
+++ src/varliable.ts
@@ -0,0 +1,15 @@
+//var scope는 함수 scope
+var score1 = 0;
+//let const는 Block scope
+let score2 = 200;
+const defaultScore: number = 0;
+const test: string = "test";
+
+
+// function outer() {
+//     //이 위치에서는 이 함수 안 어디서든 사용가능
+//     //var score = 0;
+//     function inner() {
+
+//     }
+// }
